ba62b259259465ea66eeb99dc5e22207d8fd33d4
Bernard Towindo <bernardtowindo38@gmail.com>
2023-08-19 17:07:38 +0200

    New Commit

---
+++ src/main/java/com/bento/fullstackBackend/exception/UserNotFoundException.java
@@ -0,0 +1,11 @@
+package com.bento.fullstackBackend.exception;
+
+public class UserNotFoundException extends RuntimeException {
+
+    public UserNotFoundException(Long id){
+
+        super("Could not find user with id:" +id);
+
+
+    }
+}
